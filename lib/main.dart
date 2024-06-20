8fd83788289eeb85b63f9f674ddabe5027932492
seongukHEO <heoseonguk3@gmail.com>
2024-06-20 22:05:22 +0900

    feat : 2차 강의

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:instagramflutter/style.dart';
 
 void main() {
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
     //그니까 테마를 따로 정해준다
     //안드로이드도 이미 만들어져있는 theme를 사용한다
-    theme: ThemeData(
-      iconTheme: IconThemeData(color: Colors.blue),
-      appBarTheme: AppBarTheme(color: Colors.white, actionsIconTheme: IconThemeData(color: Colors.black)),
-      textTheme: TextTheme(
-        bodyMedium: TextStyle(color: Colors.red)
-      )
-
-    ),
+    theme: theme,
     home: MyApp())
   );
 }
@@ -24,12 +18,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          Icon(Icons.add_box_outlined)
-        ],
         title: Text("Instagram"),
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.add_box_outlined),
+              iconSize: 30,
+          )
+        ],
       ),
-      body: Text("dk"),
+      body: Text('안녕'),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.home_outlined),
+                iconSize: 30,
+            ),
+            IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.shopping_bag_outlined),
+                iconSize: 30,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+var theme = ThemeData(
+    iconTheme: IconThemeData(color: Colors.black),
+    appBarTheme: AppBarTheme(
+        color: Colors.white,
+        //그림자
+        elevation: 1,
+        titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+        actionsIconTheme: IconThemeData(color: Colors.black)),
+    textTheme: TextTheme(
+        bodyMedium: TextStyle(color: Colors.red)
+    )
+
+);
