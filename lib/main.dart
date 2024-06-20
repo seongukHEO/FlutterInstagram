8d525eed0dc30e7779da8ec1924f64ef94d70afa
seongukHEO <heoseonguk3@gmail.com>
2024-06-20 21:43:50 +0900

    feat : 1차 강의

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+    //그니까 테마를 따로 정해준다
+    //안드로이드도 이미 만들어져있는 theme를 사용한다
+    theme: ThemeData(
+      iconTheme: IconThemeData(color: Colors.blue),
+      appBarTheme: AppBarTheme(color: Colors.white, actionsIconTheme: IconThemeData(color: Colors.black)),
+      textTheme: TextTheme(
+        bodyMedium: TextStyle(color: Colors.red)
+      )
+
+    ),
+    home: MyApp())
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +22,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Icon(Icons.add_box_outlined)
+        ],
+        title: Text("Instagram"),
+      ),
+      body: Text("dk"),
+    );
   }
 }
