6cff7d3149923e3a477631ea898af6f338dc50f1
seongukHEO <heoseonguk3@gmail.com>
2024-06-20 23:31:40 +0900

    feat : 4차 강의

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagramflutter/style.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
@@ -20,6 +22,24 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var tab = 0;
+
+
+  getData() async{
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    //jsonDecode는 받아온 데이터를 리스트 형태로 만들어준다
+    var result2 = jsonDecode(result.body);
+    print(result2[0]['likes']);
+  }
+
+
+
+  //안드로이드의 init 즉 MyApp이 로드될때 바로 인스턴스에 올라간다
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
 
 
   @override
