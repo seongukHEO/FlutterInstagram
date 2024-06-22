4898039698d9f1fdeea41c513dc5727530218206
seongukHEO <heoseonguk3@gmail.com>
2024-06-22 22:44:00 +0900

    feat : 14차 강의

--- lib/profile.dart
+++ lib/profile.dart
@@ -13,31 +13,48 @@ class Profile extends StatelessWidget {
       appBar: AppBar(
         title: Text(context.watch<Store1>().name),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            CircleAvatar(
-              radius: 30,
-              backgroundColor: Colors.grey,
-            ),
-            Text('팔로워 : ${context.watch<Store1>().follower}명'),
-            ElevatedButton(
-                onPressed: (){
-                  context.read<Store1>().changeFollower();
-                },
-                child: Text('팔로우')
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  context.read<Store1>().getData();
-                },
-                child: Text('사진 가져오기')
-            )
-          ],
-        ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: ProfileHeader(),
+          ),
+          SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                  (c, i) => Image.network(context.watch<Store1>().profileImg[i]),
+                childCount: context.watch<Store1>().profileImg.length
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2)
+          )
+        ],
       )
     );
   }
 }
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.grey,
+          ),
+          Text('팔로워 : ${context.watch<Store1>().follower}명'),
+          ElevatedButton(
+              onPressed: (){
+                context.read<Store1>().changeFollower();
+              },
+              child: Text('팔로우')
+          ),
+        ],
+      ),
+    );
+  }
+}
+
